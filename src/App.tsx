c94db189b8306f901650bf0a292587a551de8554
Shahzeb K <1383831+shahzeb1@users.noreply.github.com>
2019-07-01 11:47:31 -0700

    This should hopefully work
    I have used the "Getting started" page to make the changes: https://github.com/intljusticemission/react-big-calendar/blob/f478be078e84224d155380f526b3790c80631aea/examples/Intro.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import BigCalendar from 'react-big-calendar'
-// This line causes an error
-// (Attempted import error: 'react-big-calendar' does not contain a default export (imported as 'BigCalendar'))
-// using "import Calendar from 'react-big-calendar" results in a different error, saying
-// Attempted import error: 'react-big-calendar' does not contain a default export (imported as 'BigCalendar')
+import { Calendar, momentLocalizer } from 'react-big-calendar'
 
 import moment from 'moment'
 import events from './events'
 
-const localizer = BigCalendar.momentLocalizer(moment);
+const localizer = momentLocalizer(moment);
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <BigCalendar
+      <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
